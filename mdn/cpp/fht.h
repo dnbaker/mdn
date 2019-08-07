01c2761dc869d850b83de9cda79bd4b939eb73fa
Daniel Baker <daniel.n.baker@ibm.com>
2019-08-07 14:31:46 -0700

    Our results are actually not good.

--- mdn/cpp/fht.h
+++ mdn/cpp/fht.h
@@ -2,10 +2,12 @@
 #include <cstring>
 #include <stdexcept>
 #include <cstdio>
+#include <algorithm>
 #include <cmath>
 #include <cassert>
 #include <chrono>
 #include <vector>
+#include <cooperative_groups.h>
 
 namespace fht {
 template<typename T>void show(const T&x) {
@@ -27,7 +29,7 @@ template<typename T>void show(const T *x, size_t n) {
 using std::size_t;
 
 template<typename T>
-void dumbfht(T *const ptr, const size_t l2) {
+size_t dumbfht(T *const ptr, const size_t l2) {
     auto start = std::chrono::high_resolution_clock::now();
     size_t n = 1ull << l2;
     for(size_t i = 0; i < l2; ++i) {
@@ -44,7 +46,7 @@ void dumbfht(T *const ptr, const size_t l2) {
         }
     }
     auto end = std::chrono::high_resolution_clock::now();
-    std::fprintf(stderr, "cpu took %zu time for %zu \n", (end - start).count(), l2);
+    return (end - start).count();
 }
 #if 0
 template<typename T>
@@ -62,51 +64,66 @@ __global__ void dumbfht_gpu_kernel(T *ptr, size_t l2, int nthreads) {
         int npert = n / nthreads_active;
         if(tid < nthreads_active) {
             for(int j = tid * npert, e = j + npert; j != e; j += s2) {
-                #pragma unroll
+                #pragma unroll 8
                 for(size_t k = 0; k < s1; ++k) {
                     auto u = ptr[j + k], v = ptr[j + k + s1];
                     ptr[j + k] = u + v, ptr[j + k + s1] = u - v;
                 }
             }
         }
-        __syncthreads();
     }
-#if 0
-    for(size_t i = 0; i < l2; ++i) {
-        int max_thread_num = 1 << (l2 - i);
-        if(i < max_thread_num) {
-            size_t s1 = 1ull << i, s2 = s1 << 1;
-            #pragma unroll
-            for(int j = start_index; j < stop_index; j += s2) {
-                #pragma unroll
-                for(size_t k = 0; k < s1; ++k) {
-                    auto u = ptr[j + k], v = ptr[j + k + s1];
-                    ptr[j + k] = u + v, ptr[j + k + s1] = u - v;
+}
+template<typename T>
+__global__ void mkdumbfht_gpu_kernel(T *ptr, size_t l2, int nthreads, int nkern) {
+    int tid = blockIdx.x*blockDim.x + threadIdx.x;
+    int n = 1 << l2;
+    for(int i = 0; i < l2; ++i) {
+        const int s1 = 1 << i, s2 = s1 << 1;
+        const int nthreads_active = min(n >> (i + 1), nthreads);
+        const int npert = n / nthreads_active;
+        for(int kind = 0; kind < nkern; ++kind) {
+            auto lptr = ptr + kind * n;
+            if(tid < nthreads_active) {
+                #pragma unroll 8
+                for(int j = tid * npert, e = j + npert; j != e; j += s2) {
+                    #pragma unroll 8
+                    for(size_t k = 0; k < s1; ++k) {
+                        auto u = lptr[j + k], v = lptr[j + k + s1];
+                        lptr[j + k] = u + v, lptr[j + k + s1] = u - v;
+                    }
                 }
             }
         }
-        __syncthreads();
     }
-#endif
 }
 
+
 template<typename T>
-void call_dumbfht(T *ptr, size_t l2, size_t threadl2 = 4, size_t blocksl2=6) {
+void call_dumbfht(T *ptr, size_t l2, size_t threadl2 = 8, int n=1) {
     size_t threads_per_block = 1ull << threadl2;
     size_t sz = 1ull << l2,
-           nbytes = sizeof(T) << l2;
-    size_t blocks = 1ull << blocksl2;
-    int ptl2 = l2 - threadl2 - blocksl2;
+           nbytes = (n * sizeof(T)) << l2;
+    size_t blocks = size_t(n) << (l2 - threadl2);
+    int ptl2 = l2 - threadl2;
     size_t items_per_thread = 1ull << ptl2;
-    if(l2 < threadl2 + blocksl2) throw 1;
     cudaError_t rc;
     T *tmp;
     if(cudaMalloc((void **)&tmp, nbytes)) throw std::bad_alloc();
     if(cudaMemcpy(tmp, ptr, nbytes, cudaMemcpyHostToDevice)) throw std::runtime_error("Failed to copy to device");
     std::fprintf(stderr, "tpb: %zu. ipt: %zu sz: %zu. blocks: %zu\n", threads_per_block, items_per_thread, sz, blocks);
     auto start = std::chrono::high_resolution_clock::now();
-    dumbfht_gpu_kernel<<<blocks, threads_per_block>>>(tmp, l2, threads_per_block * blocks);
+#if 0
+    cudaStream_t cs[16];
+    std::for_each(std::begin(cs), std::end(cs), [](auto &x) {cudaStreamCreate(&x);});
+    for(int i = 0; i < n; ++i) {
+        dumbfht_gpu_kernel<<<blocks, threads_per_block, 0, cs[i % 16]>>>(tmp + (size_t(i) << l2), l2, threads_per_block * blocks);
+    }
+    std::for_each(std::begin(cs), std::end(cs), [](auto x) {cudaStreamDestroy(x);});
+#else
+    mkdumbfht_gpu_kernel<<<blocks, threads_per_block>>>(tmp, l2, threads_per_block * blocks, n);
+#endif
     //dumbfake(tmp, l2, items_per_thread, blocks, threads_per_block);
+    cudaDeviceSynchronize();
     auto end = std::chrono::high_resolution_clock::now();
     if(cudaMemcpy(ptr, tmp, nbytes, cudaMemcpyDeviceToHost)) throw "thing";
     std::fprintf(stderr, "gpu took %zu time for %zu \n", (end - start).count(), l2);
--- mdn/cpp/fhttest.cu
+++ mdn/cpp/fhttest.cu
@@ -1,10 +1,12 @@
 #include "fht.h"
 #include <random>
+#include "omp.h"
 using namespace fht;
 
 int main(int argc, char *argv[]) {
-    int l2 = argc == 1 ? 16: std::atoi(argv[1]), subl2 = 4;
-    std::vector<double> zomg(1 << l2);
+    int l2 = argc == 1 ? 16: std::atoi(argv[1]), subl2 = 8;
+    int n = 256;
+    std::vector<double> zomg(n << l2);
     std::normal_distribution<float> gen;
     std::mt19937_64 mt;
     for(auto &e: zomg) e = gen(mt);
@@ -21,13 +23,27 @@ int main(int argc, char *argv[]) {
         z2 = zomg;
     }
 #endif
-    dumbfht(z2.data(), l2);
+    size_t s = 0;
+    omp_set_num_threads(48);
+    auto start = std::chrono::high_resolution_clock::now();
+    #pragma omp parallel reduction(:+s)
+    for(int i = 0; i < n; ++i) {
+        dumbfht(z2.data() + (i << l2), l2);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::fprintf(stderr, "cpu took %zu\n", (end - start).count());
     show(z2);
     show(zomg);
-    call_dumbfht(zomg.data(), l2, subl2);
+    call_dumbfht(zomg.data(), l2, subl2, n);
     show(zomg);
     double sum = 0.;
     for(size_t i = 0; i < z2.size(); ++i)
         sum += std::abs(z2[i] - zomg[i]);
-    assert(z2 == zomg || std::fprintf(stderr, "Failed: %lf", sum / z2.size()));
+    double md = 0.;
+    if(z2 != zomg) {
+        for(size_t i = 0; i < z2.size(); ++i)
+            if(md < std::abs(z2[i] - zomg[i]))
+                md = std::abs(z2[i] - zomg[i]), std::fprintf(stderr, "Max difference: %f/%f:%f\n", z2[i], zomg[i], abs(z2[i] - zomg[i]));
+    }
+    assert(z2 == zomg || (sum / z2.size() < 10.) || !std::fprintf(stderr, "Failed: %lf", sum / z2.size()));
 }
